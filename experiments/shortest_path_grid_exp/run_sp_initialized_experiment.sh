1a82e5a816ca361c7bb3b677e2cc67c341c613f3
vgupta1 <guptavis@usc.edu>
2026-02-21 20:27:03 -0800

    Updating batch file

--- experiments/shortest_path_grid_exp/run_sp_initialized_experiment.sh
+++ experiments/shortest_path_grid_exp/run_sp_initialized_experiment.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-  #SBATCH --array=0-2399%100
-  #SBATCH --ntasks=1
-  #SBATCH --cpus-per-task=1
-  #SBATCH --mem-per-cpu=4GB
-  #SBATCH --time=45:00:00
-  #SBATCH --export=none
-  #SBATCH --job-name=pg_sp_experiment_initialized
-  #SBATCH --mail-user=guptavis@usc.edu
+#SBATCH --array=0-2399%100
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=4GB
+#SBATCH --time=45:00:00
+#SBATCH --export=none
+#SBATCH --job-name=pg_sp_experiment_initialized
+#SBATCH --mail-user=guptavis@usc.edu
 
-  module purge
-  module load ver/2506
-  module load gcc/14.3.0
-  module load python/3.11.14
+module purge
+module load ver/2506
+module load gcc/14.3.0
+module load python/3.11.14
 
-  # Go to repo root
-  cd /scratch1/guptavis/decision-focused-learning/experiments/shortest_path_grid_exp
-  python3 sp_experiment_initialized_slurm.py $SLURM_ARRAY_TASK_ID
+# Go to repo root
+cd /scratch1/guptavis/decision-focused-learning/experiments/shortest_path_grid_exp
+
+# Required by sp_experiment_initialized_slurm.py
+export BASELINE_RUN_ID=6922354
+
+python3 sp_experiment_initialized_slurm.py $SLURM_ARRAY_TASK_ID
